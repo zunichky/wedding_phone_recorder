6eddcffcdbaa52d3b6734ee161050cf146707c54
zunichky <zunichky@msu.edu>
2024-02-02 19:13:25 -0500

    first attempt at video + audio

--- phone_guest_book/OS/Audio/Recording/LinuxRecorder.cs
+++ phone_guest_book/OS/Audio/Recording/LinuxRecorder.cs
@@ -4,14 +4,15 @@
     {
         protected override string GetBashCommand(string fileName)
         {
-            string command = $"arecord -vv --format=cd ";
-
+            // libcamera-vid --codec libav --vflip --width 1920 --height 1080 --bitrate 7000000 --libav-audio --audio-device alsa_input.usb-Logitech_Logi_USB_Headset-00.mono-fallback -o video.mp4
+            //string command = $"arecord -vv --format=cd ";
+            string command =  "libcamera-vid --codec libav --vflip --width 1920 --height 1080 --bitrate 7000000 --libav-audio --audio-device alsa_input.usb-Logitech_Logi_USB_Headset-00.mono-fallback -o ";
             if (Path.GetExtension(fileName).ToLower().Equals(".mp3"))
             {
                 command += "--file-type raw | lame -r - ";
             }
 
-            return command += fileName;
+            return command + fileName;
         }
     }
 }
