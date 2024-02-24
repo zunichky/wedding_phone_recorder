4a1d621d464dfeb6ae0f242357c007f878ee649d
zunichky <zunichky@msu.edu>
2024-02-23 19:27:22 -0500

    Updated audio device

--- phone_guest_book/OS/Audio/Recording/LinuxRecorder.cs
+++ phone_guest_book/OS/Audio/Recording/LinuxRecorder.cs
@@ -7,14 +7,15 @@
             //string command = $"arecord -vv --format=cd ";
             string command =  "libcamera-vid -t 0 -c --codec libav -v 0 " +
                               "--vflip --width 1920 --height 1080 --bitrate 7000000 " +
-                              "--libav-audio --audio-device alsa_input.usb-Generic_AB13X_USB_Audio_20210726905926-00.mono-fallback -o ";
+                              "--libav-audio --audio-source alsa --audio-device hw:CARD=Audio --audio-channels 1 " +
+                              "-o " + fileName;
 
             if (Path.GetExtension(fileName).ToLower().Equals(".mp3"))
             {
                 command += "--file-type raw | lame -r - ";
             }
 
-            return command += fileName;
+            return command;
         }
     }
 }
