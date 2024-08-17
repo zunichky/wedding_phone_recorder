7ea3e64eeb072d352866814d3a32bca37e6e53c7
zunichky <zunichky@msu.edu>
2024-08-16 21:07:16 -0400

    flip camera

--- phone_guest_book/OS/Audio/Recording/LinuxRecorder.cs
+++ phone_guest_book/OS/Audio/Recording/LinuxRecorder.cs
@@ -8,7 +8,7 @@ namespace phone_guest_book.OS.Audio.Recording
         {
             //string command = $"arecord -vv --format=cd ";
             string command =  "libcamera-vid -t 0 -c --codec libav -v 0 " +
-                              "--vflip --width 1920 --height 1080 --bitrate 7000000 " +
+                              " --width 1920 --height 1080 --bitrate 7000000 " +
                               "--libav-audio --audio-source alsa --audio-device hw:CARD=Device --audio-channels 1 " +
                               "-o " + fileName;
 
